140fb523633170924ac17ddf909e729cec956701
Jan Amann <jan@amann.me>
2023-03-01 17:00:59 +0100

    Add caching

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,11 +1,27 @@
+import ms from 'ms';
 import createIntlMiddleware from 'next-intl/middleware';
+import {NextRequest} from 'next/server';
 
-export default createIntlMiddleware({
+const middleware = createIntlMiddleware({
   locales: ['en', 'es'],
   defaultLocale: 'en'
 });
 
+export default function handler(req: NextRequest) {
+  const response = middleware(req);
+
+  response.headers.set(
+    'Cache-Control',
+    [
+      `s-maxage=` + ms('1d') / 1000,
+      `stale-while-revalidate=` + ms('1y') / 1000
+    ].join(', ')
+  );
+
+  return response;
+}
+
 export const config = {
-  // Skip all internal paths
-  matcher: ['/((?!_next).*)']
+  // Skip all non-content paths
+  matcher: ['/((?!_next|favicon.ico).*)']
 };
